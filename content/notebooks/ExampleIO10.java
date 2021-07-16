c8867933bda41993d2f423f1db124b761aa56087
Henry Arcila <helpu1409@gmail.com>
2021-07-16 01:06:07 -0500

    actualizando notebook condicional

---
+++ content/notebooks/ExampleIO10.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Calificacion {
+    public static void main(String[] args) {
+        // Variables
+        float score;
+        char grade;
+
+
+        // Objeto tipo Scanner
+        Scanner input = new Scanner(System.in);  // Declaracion de un objeto (llamado teclado) de la clase Scanner
+
+        //Ingreso de la nota numerica
+        System.out.print("Ingrese la nota numerica (dentro del rango [0,100]): ");
+        score = input.nextFloat();
+
+        // Validación de la nota numerica y obtencion de la nota en letras dependiendo la tabla
+        if (score >= 90.0) {
+            grade = 'A';
+        }
+        else {
+            if (score >= 80.0) {
+                grade = 'B';
+            }
+            else {
+                if (score >= 70.0) {
+                    grade = 'C';
+                }
+                else {
+                    if (score >= 60.0) {
+                        grade = 'D';
+                    }
+                    else {
+                        grade = 'F';
+                    }
+                }
+            }
+        }
+    }
+}
